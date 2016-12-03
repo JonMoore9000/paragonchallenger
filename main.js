749a9b269eb9ab9e0c386778885d7a2a2e870096
JonMoore9000 <jonmoore9000@gmail.com>
2016-12-03 17:28:39 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,166 @@
+var challenges = 
+    
+    [
+    
+//Countess
+{char: 'Countess', challenge:'As Countess, kill an enemy under tower with Shadow Slip before the 6 minute mark and survive.'},
+{char: 'Countess', challenge:'As Countess, get a triple kill with Dark Tide.'},
+{char: 'Countess', challenge:'As Countess, kill an enemy in mid-air with Feast.'},
+ 
+
+//Dekker
+{char: 'Dekker', challenge:'As Dekker, kill an opponent with Stasis Bomb at max range.'},
+{char: 'Dekker', challenge:'As Dekker, clear an entire enemy wave with just Stasis Bomb.'},
+{char: 'Dekker', challenge:'As Dekker, steal the last hit of a kill with Slow Bubble.'},
+{char: 'Dekker', challenge:'As Dekker, use Rocket Boots to boost your entire teams speed and chase a kill.'},
+ 
+
+//Feng Mao
+{char: 'Feng Mao', challenge:'As Feng Mao, hit the entire enemy team with a single use of Hamstring.'},
+{char: 'Feng Mao', challenge:'As Feng Mao, get a double kill with a single use of Hamstring.'},
+{char: 'Feng Mao', challenge:'As Feng Mao, steal Orb Prime with Reaping Dash into Earth Shatter and survive the escape.'},
+
+
+//Gadget
+{char: 'Gadget', challenge:'As Gadget, kill an enemy with a Sticky Mine attached to yourself.'},
+{char: 'Gadget', challenge:'As Gadget, get a double kill ( or higher) with a single Sticky Mine.'},
+{char: 'Gadget', challenge:'As Gadget, fight an enemy under Seek & Destroy and win. (Bonus points for fighting more than one enemy).'},
+{char: 'Gadget', challenge:'As Gadget, kill an enemy with the damage from Speed Gate.'},
+{char: 'Gadget', challenge:'As Gadget, get a triple kill or higher with Tesla Dome.'},
+
+
+
+//Gideon
+{char: 'Gideon', challenge:'As Gideon, get a triple kill or higher with Cosmic Rift.'},
+{char: 'Gideon', challenge:'As Gideon, save an ally by having him travel through your Torn Space portal.'},
+{char: 'Gideon', challenge:'As Gideon, Use Burden, into Cosmic Rift, and then Torn Space into a Black Hole to secure a kill.'},
+{char: 'Gideon', challenge:'As Gideon, damage the entire enemy team with one use of Black Hole.'},
+ 
+
+//Greystone
+{char: 'Greystone', challenge:'As Greystone, get a double kill with Assault the Gates.'},
+{char: 'Greystone', challenge:'As Greystone, knock up the entire enemy team with Reforged and survive the escape.'},
+{char: 'Greystone', challenge:'As Greystone, 1v2 and win with Reforged on cooldown.'},
+ 
+ 
+//Grim.exe
+{char: 'GRIM.exe', challenge:'As GRIM.exe, deflect a killing blow with Deflector Shield and survive the escape.'},
+{char: 'GRIM.exe', challenge:'As GRIM.exe, knock away the entire enemy team with Displacement Blast.'},
+{char: 'GRIM.exe', challenge:'As GRIM.exe, kill an enemy with GTFO that wasnt your original target.'},
+
+
+//Grux
+{char: 'Grux', challenge:'As Grux, use your Smash and Grab ability to pull in two enemy heroes and then get a double kill with your Double Pain ability.'},
+{char: 'Grux', challenge:'As Grux, combo Charge, Smash and Grab and Double Pain and get a kill.'},
+{char: 'Grux', challenge:'As Grux, pull an entire team with Smash and Grab then stun all of them with Warlords Challenge.'},
+ 
+ 
+//Howitzer
+{char: 'Howitzer', challenge:'As Howitzer, get a triple kill with your Make It Rain ability.'},
+{char: 'Howitzer', challenge:'As Howitzer, save a teammate from certain death with your LandMine ability.'},
+{char: 'Howitzer', challenge:'As Howitzer, slow an enemy with Slow Grenades, then kill them with R2000 Missile.'},
+ 
+ 
+//Iggy and Scorch
+{char: 'Iggy and Scorch', challenge:'As Iggy and Scorch, combo Oil Spill ability with Molotov to score a double kill.'},
+{char: 'Iggy and Scorch', challenge:'As Iggy and Scorch, while escaping using Oil Slick get a kill with your Molotov ability.'},
+{char: 'Iggy and Scorch', challenge:'As Iggy and Scorch, use your Flame Turret to kill an enemy under their own tower.'},
+
+
+//Kallari
+{char: 'Kallari', challenge:'As Kallari, use your Death Sentence ability to kill an enemy hero and save a fellow teammate.'},
+{char: 'Kallari', challenge:'As Kallari, use your Shadow Walk ability to sneak up on an enemy gathering a harvester and kill them.'},
+{char: 'Kallari', challenge:'As Kallari, combo your Shadow Walk , and Crippling Dagger to steal Orb Prime from the enemy team.'},
+
+
+//Khaimera
+{char: 'Khaimera', challenge:'As Khaimera, tank the Prime Guardian, solo, before the ten minute mark.'},
+{char: 'Khaimera', challenge:'As Khaimera, kill an enemy Rampage while Rampage is Enraged by yourself.'},
+
+
+//Kwang
+{char: 'Kwang', challange:'As Kwang, get a double kill by rooting two players with Judgement of Heavens then killing them with Light of the Heavens.'},
+{char: 'Kwang', challange:'As Kwang, with your sword rooted, use your Fury of the Heavens ability to teleport to your sword in another lane and get a kill.'},
+
+
+//Lt. Belica
+{char: 'Lt. Belica', challenge:'As Lt. Belica, get a double kill using your Void Bomb ability.'},
+{char: 'Lt. Belica', challenge:'As Lt. Belica, knock up an enemy with Seismic Assault, then kill them with Neural Disruptor.'},
+
+
+//Murdock
+{char: 'Murdock', challenge:'As Murdock, use Long Arm of the Law from base and get a kill.'},
+{char: 'Murdock', challenge:'As Murdock, use your Static Trap to slow a fleeing enemy then kill them with BuckShot.'},
+{char: 'Murdock', challenge:'As Murdock, get a double kill with Long Arm of the Law.'},
+
+
+//Muriel
+{char: 'Muriel', challenge:'As Muriel save an allied hero with Alacrity.'},
+{char: 'Muriel', challenge:'As Muriel, get a double kill with your Reversal of Fortune ability.'},
+{char: 'Muriel', challenge:'As Muriel,  use Consecrated Ground to get either two kills or two assists.'},
+
+
+//Narbash
+{char: 'Narbash', challenge:'As Narbash, kill an enemy while they are under tower with Thunk.'},
+{char: 'Narbash', challenge:'As Narbash, get a double kill using Crash Bang Boom.'},
+{char: 'Narbash', challenge:'As Narbash, apply your March ability to your entire team while storming a tower.'},
+ 
+ 
+//Rampage
+{char: 'Rampage', challenge:'As Rampage, kill an enemy before unlocking your abilities.'},
+{char: 'Rampage', challenge:'As Rampage, steal Orb Prime using Enraged into a Pounce from either invisibility pool near Orb Prime.'},
+{char: 'Rampage', challenge:'As Rampage, kill an enemy under tower with Boulder Throw before the 6 minute mark.'},
+
+
+//Riktor
+{char: 'Riktor', challenge:'As Riktor, pull an enemy from under their tower with Riplash then kill them.'},
+{char: 'Riktor', challenge:'As Riktor, silence an enemy with Shock Therapy, then pull then in with Riplash then kill then with Skewer.'},
+{char: 'Riktor', challenge:'As Riktor, kill an enemy with Electrocute.'},
+
+
+//Sevarog
+{char: 'Sevarog', challenge:'As Sevarog, combo Subjugate and Phantom Rush to root an enemy, catch up to them and kill them.'},
+{char: 'Sevarog', challenge:'As Sevarog, get a double kill with Colossal Blow.'},
+{char: 'Sevarog', challenge:'As Sevarog, kill an enemy with Subjugate.'},
+
+
+//Sparrow
+{char: 'Sparrow', challenge:'As Sparrow, kill an enemy with Piercing Shot.'},
+{char: 'Sparrow', challenge:'As Sparrow, kill an enemy hero under their tower with Hail of Arrows.'},
+{char: 'Sparrow', challenge:'As Sparrow, get a double kill with Inner Fire.'},
+
+
+//Steel
+{char: 'Steel', challenge:'As Steel, knock up an entire enemy team with Shield Slam.'},
+{char: 'Steel', challenge:'As Steel, get a double kill with your Charge ability.'},
+{char: 'Steel', challenge:'As Steel, use your Force Shield ability to save two allied heroes.'},
+
+
+//The Fey
+{char: 'The Fey', challenge:'As The Fey, grab an entire team with Fly Trap.'},
+{char: 'The Fey', challenge:'As The Fey, slow and enemy with Bramble Patch then kill them with Harvest Nettles.'},
+{char: 'The Fey', challenge:'As The Fey, get a double kill with Untamed Growth.'},
+
+
+//Twinblast
+{char: 'Twinblast', challenge:'As TwinBlast, avoid an ability from an enemy hero with Rocket Dash then kill them.'},
+{char: 'Twinblast', challenge:'As TwinBlast, get a double kill with your Barrage ability.'},
+{char: 'Twinblast', challenge:'As Twinblast, combo your Grenade and Nitro ability to get a kill.'},
+
+
+//Crunch
+{char: 'Crunch', challenge:'As Crunch, use your Re-Crunch ability and kill an enemy.'},
+{char: 'Crunch', challenge:'As Crunch, use all four abilities in combo to get a kill.'},
+{char: 'Crunch', challenge:'As Crunch, use Forward Crunch to catch a fleeing enemy then kill them.'},
+
+];
+      
+$('#selector').click(function() {
+    var selected = $('#character').val();
+    var filtered = challenges.filter(function(item) {
+        return item.char === selected;
+    });
+    var random = Math.floor( Math.random() * filtered.length );
+    var finalChallenge = filtered[random].challenge;
+    $('#challengeForYou').text(finalChallenge);
+});
